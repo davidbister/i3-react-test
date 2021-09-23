e4d2a1a86b344cf2136ec0eca50df2cecaf22bbb
davidbister <davidbister1234@gmail.com>
2021-09-23 16:37:25 +0200

    data fetching trough axios 1/2

--- i3-react-test/src/components/pages/FirstPage.js
+++ i3-react-test/src/components/pages/FirstPage.js
@@ -1,13 +1,37 @@
 //rfce
-import React from 'react';
+import React,{useState,useEffect} from 'react'
 import '../../App.scss';
 
+import axios from 'axios';
+
+//I need to imporve this part of code,i will make custom hook :)
 function FirstPage() {
+    const[post,setPosts]=useState([]);
+
+    useEffect(()=>{
+axios.get('http://localhost:8000/data').then(res=>{
+    console.log(res)
+    setPosts(res.data)
+})
+.catch(err=>{
+    console.log(err)
+})
+    },[])
+    //
+    
     return (
-        <>
-            <h1 className="FirstPage">FirstPage</h1>
-        </>
-    )
+        <div>
+            
+        <ul>
+            {post.map(post=>(
+                <li key={post.id}>{post.title} <img src={post.pic}alt={post.userId} width={200} height={200} /></li>
+               
+
+            ))}
+        </ul>
+        
+    </div>
+    );
 }
 
 export default FirstPage
