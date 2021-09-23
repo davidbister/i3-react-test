34d3a928663203635b5aa93d544f34b08aaa397c
davidbister <davidbister1234@gmail.com>
2021-09-23 09:53:24 +0200

    adding components and pages(blank)

---
+++ i3-react-test/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React,{useState} from 'react';
+import {Link} from 'react-router-dom';
+import './Navbar.scss';
+
+function Navbar() {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Navbar
