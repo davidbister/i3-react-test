ef85a6c3da7b213c841cf88bbe353bbfbce8217e
davidbister <davidbister1234@gmail.com>
2021-09-24 14:44:14 +0200

    styling are not done jet

--- i3-react-test/src/components/pages/SecondPage.js
+++ i3-react-test/src/components/pages/SecondPage.js
@@ -1,6 +1,7 @@
 //rfce
 import React,{useRef} from 'react';
-import '../../App.scss';
+import'./SecondPage.scss';
+
 import { useFetch } from '../useFetch';
 
 const SecondPage=()=> {
@@ -11,9 +12,16 @@ const SecondPage=()=> {
       isComponentMounted,
       []
     );
+
     
-    let divNameSp="SecondPage";
-    let pNameSp="pSecondPage";
+    let imgSmall="imgsmall";
+    let imgSmall3="imgsmall3";
+    let imgBig="imgbig";
+    let wrapper="wrapper";
+    let box1="box1";
+    let box2="box2";
+    let box3="box3";
+    let box4="box4";
 
     return (
         <>
@@ -21,27 +29,35 @@ const SecondPage=()=> {
         <div>Loading data...</div>
       ) : (
         data.map((data) => (
-            <div key={data.id} className={divNameSp}>
-              <h3>{data.title}</h3>
-              <p className={pNameSp}>{data.body}</p>
-              <img src={data.img1} alt={data.userId} width={data.imgWidth1} height={data.imgHeight1} />
-              <img src={data.img2} alt={data.userId} width={data.imgWidth} height={data.imgHeight} />
-              <img src={data.img3} alt={data.userId} width={data.imgWidth} height={data.imgHeight} />
-              <img src={data.img3} alt={data.userId} width={data.imgWidth} height={data.imgHeight} />
-              
+            <div className={wrapper} key={data.id} >
+
+              <div className={box1}>{data.title}</div>
+              <div className={box2}> {data.body}</div>
              
-             
-            
+             <div className={box3}>
+                 
+                  <img className={imgSmall} src={data.img1} alt={data.userId} width={data.imgWidth1} height={data.imgHeight1} 
+             />
+             <img className={imgSmall} src={data.img1} alt={data.userId} width={data.imgWidth1} height={data.imgHeight1} 
+             /><img className={imgSmall3} src={data.img1} alt={data.userId} width={data.imgWidth1} height={data.imgHeight1} 
+             /></div>
+             <div className={box4}>
+              <img clasName={imgBig} src={data.img2} alt={data.userId} width={data.imgWidth} height={data.imgHeight} />
+
+              </div>
+
+
+
             </div>
         ))
-     
+
 
       )}
 
 
-       
-     
-        
+
+
+
         </>
     );
 };
